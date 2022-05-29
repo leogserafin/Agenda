3d9154475a8f9129e54c3496ea86e9652bf49f11
Marco Corazza <51054097+MarkCrz@users.noreply.github.com>
2022-05-29 18:34:24 -0300

    Tela de Cadastro

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.database.Cursor;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.example.agenda.database.DBHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
@@ -15,9 +18,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ListView lvAgenda;
-    private FloatingActionButton btn_add;
-    private DBHelper db;
-    private ArrayList<String> listaAgenda;
+    private AppCompatButton btn_add;
+    private AgendaAdicao agendaAdicao = new AgendaAdicao();
 
 
     @Override
@@ -27,43 +29,30 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         IniciarComponentes();
 
-        db=new DBHelper(this);
+        ArrayList<String> listaAgenda = new ArrayList<>();
 
-        listaAgenda = new ArrayList<>();
-
-       listarContatos();
-
-       /*
         listaAgenda.add("Bruno");
         listaAgenda.add("Leonardo");
         listaAgenda.add("Marco");
         listaAgenda.add("Matheus");
-*/
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_list_item_1, listaAgenda
         );
 
         lvAgenda.setAdapter(adapter);
-    }
 
-    private void listarContatos() {
-        Cursor c = db.SelectAll_Contato();
-        c.moveToFirst();
-        if(c.getCount()>0){
-            listaAgenda.clear();
-            do{
-                int id = c.getInt(c.getColumnIndex("id"));
-                String nome = c.getString(c.getColumnIndex("nome"));
-                String tel = c.getString(c.getColumnIndex("tel"));
-                int tipo = c.getInt(c.getColumnIndex("tipo"));
-
-                listaAgenda.add(nome);
-            }while(c.moveToNext());
-        }
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.MainFrameLayout, agendaAdicao).commit();
+            }
+        });
     }
 
     private void IniciarComponentes(){
         lvAgenda = findViewById(R.id.listViewAgenda);
-        btn_add = findViewById(R.id.floatingActionButtonAdd);
+        btn_add = findViewById(R.id.btnCadastrar);
     }
 }
